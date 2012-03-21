b7af67c51a2c44ba50ba4f454b06608bd17dde1f
Christopher Hlubek <hlubek@networkteam.com>
2012-03-21 13:33:19 +0100

    [!!!][TASK] CGL update, Textcat is a singleton now

--- Classes/Textcat.php
+++ Classes/Textcat.php
@@ -1,39 +1,35 @@
 <?php
-declare(ENCODING = 'utf-8');
 namespace Libtextcat;
 
 /*                                                                        *
- * This script belongs to the FLOW3 package "Libtextcat".          *
+ * This script belongs to the FLOW3 package "Libtextcat".                 *
  *                                                                        *
  * It is free software; you can redistribute it and/or modify it under    *
- * the terms of the GNU Lesser General Public License as published by the *
- * Free Software Foundation, either version 3 of the License, or (at your *
- * option) any later version.                                             *
- *                                                                        *
- * This script is distributed in the hope that it will be useful, but     *
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
- * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser       *
- * General Public License for more details.                               *
- *                                                                        *
- * You should have received a copy of the GNU Lesser General Public       *
- * License along with the script.                                         *
- * If not, see http://www.gnu.org/licenses/lgpl.html                      *
+ * the terms of the GNU General Public License, either version 3 of the   *
+ * License, or (at your option) any later version.                        *
  *                                                                        *
  * The TYPO3 project - inspiring people to share!                         *
  *                                                                        */
 
+use TYPO3\FLOW3\Annotations as FLOW3;
+
 /**
- * Fingerprint functions
+ * Textcat text categorization
  *
- * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License, version 3 or later
+ * @FLOW3\Scope("singleton")
  */
 class Textcat {
 
 	const MINDOCSIZE = 25;
+
 	const MAXNGRAMSIZE = 5;
+
 	const MAXNGRAMS = 400;
+
 	const MAXOUTOFPLACE = 400;
+
 	const MAXSCORE = 2147483647;
+
 	const THRESHOLDVALUE = 1.03;
 
 	/**
@@ -47,24 +43,22 @@ class Textcat {
 	 * @var array
 	 */
 	protected $availableCategories = array(
-			'english' => 'en',
-			'german' => 'de',
-			'french' => 'fr',
-			'spanish' => 'es',
-			'italian' => 'it',
-			'portuguese' => 'pt',
-			'danish' => 'da',
-			'swedish' => 'sv',
-			'norwegian' => 'no',
-			'finnish' => 'fi'
-		);
+		'english' => 'en',
+		'german' => 'de',
+		'french' => 'fr',
+		'spanish' => 'es',
+		'italian' => 'it',
+		'portuguese' => 'pt',
+		'danish' => 'da',
+		'swedish' => 'sv',
+		'norwegian' => 'no',
+		'finnish' => 'fi'
+	);
 
 	/**
 	 * Constructor
 	 *
-	 * Initializes the categories from static .lm files.
-	 *
-	 * @author Christopher Hlubek <hlubek@networkteam.com>
+	 * Initializes the categories from static .lm files included in Resources/Private.
 	 */
 	public function __construct() {
 		$this->categories = array();
@@ -80,7 +74,6 @@ class Textcat {
 	 *
 	 * @param string $text The text to classify in UTF-8 encoding
 	 * @return string|boolean The classified language as ISO 2-letter code or FALSE if no classification could be made
-	 * @author Christopher Hlubek <hlubek@networkteam.com>
 	 */
 	public function classify($text) {
 		$fingerprint = self::create($text, self::MAXNGRAMS);
@@ -107,7 +100,6 @@ class Textcat {
 	 *
 	 * @param string $category
 	 * @return array
-	 * @author Christopher Hlubek <hlubek@networkteam.com>
 	 */
 	protected function readFingerprint($category) {
 		$fingerprint = array();
@@ -127,14 +119,12 @@ class Textcat {
 	}
 
 	/**
-	 * Compare the fingerprint of the given category against the unknown
-	 * fingerprint.
+	 * Compare the fingerprint of the given category against an unknown fingerprint.
 	 *
 	 * @param array $category
 	 * @param array $unknown
 	 * @param integer $cutoff
 	 * @return integer
-	 * @author Christopher Hlubek <hlubek@networkteam.com>
 	 */
 	static public function compare(&$category, &$unknown, $cutoff) {
 		$sum = 0;
@@ -161,7 +151,6 @@ class Textcat {
 	 *
 	 * @param string $buffer
 	 * @return array|false Fingerprint as array indexed by ngrams with score
-	 * @author Christopher Hlubek <hlubek@networkteam.com>
 	 */
 	static public function create($buffer, $maxngrams) {
 		$size = mb_strlen($buffer, 'UTF-8');
@@ -169,16 +158,16 @@ class Textcat {
 			return FALSE;
 		}
 
-			// Throw out all invalid chars
+		// Throw out all invalid chars
 		$tmp = preg_replace('/[\d\s]+/', '_', $buffer);
 
-			// Create a hash table containing n-gram counts
+		// Create a hash table containing n-gram counts
 		$table = self::createngramtable($tmp);
 
-			// Sort n-grams alphabetically, for easy comparison
+		// Sort n-grams alphabetically, for easy comparison
 		arsort($table);
 
-			// Cut-off table at MAXNGRAMS count
+		// Cut-off table at MAXNGRAMS count
 		return array_slice($table, 0, min(count($table), self::MAXNGRAMS));
 	}
 
@@ -189,27 +178,30 @@ class Textcat {
 	 *
 	 * @param string $buffer
 	 * @return array
-	 * @author Christopher Hlubek <hlubek@networkteam.com>
 	 */
 	static protected function createngramtable($buffer) {
 		$table = array();
 
-			// Get all n-grams where 1<=n<=MAXNGRAMSIZE. Allow underscores only at borders.
+		// Get all n-grams where 1<=n<=MAXNGRAMSIZE. Allow underscores only at borders.
 		$size = mb_strlen($buffer, 'UTF-8');
 		for ($j = 0; $j < $size - 1; $j++) {
 			for ($i = 1; $i <= self::MAXNGRAMSIZE; $i++) {
-				if ($j + $i > $size) break;
+				if ($j + $i > $size) {
+					break;
+				}
 				$p = mb_substr($buffer, $j, $i, 'UTF-8');
 				if (!isset($table[$p])) {
 					$table[$p] = 1;
 				} else {
 					$table[$p]++;
 				}
-				if ($i > 1 && mb_substr($p, -1, 1, 'UTF-8') === '_') break;
+				if ($i > 1 && mb_substr($p, -1, 1, 'UTF-8') === '_') {
+					break;
+				}
 			}
 		}
 		return $table;
 	}
-
 }
+
 ?>
